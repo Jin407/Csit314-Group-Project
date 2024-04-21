38150648d24f3c0314ec5254133124b90f6e233e
Jin An <Limjinan407@gmail.com>
2024-04-21 22:46:11 +0800

    Uploaded sql scripts to create sqluser in root and insert userrecords

---
+++ InsertUsers.sql
@@ -0,0 +1,11 @@
+INSERT INTO UserDatabase.Users (Username, Password, UserType)
+VALUES ('username', 'password', 'Admin');
+
+INSERT INTO UserDatabase.Users (Username, Password, UserType)
+VALUES ('buyerusername', 'buyerpassword', 'Buyer');
+
+INSERT INTO UserDatabase.Users (Username, Password, UserType)
+VALUES ('sellerusername', 'sellerpassword', 'Seller');
+
+INSERT INTO UserDatabase.Users (Username, Password, UserType)
+VALUES ('agentusername', 'agentpassword', 'REA');
